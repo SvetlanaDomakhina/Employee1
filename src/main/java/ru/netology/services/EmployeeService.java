d53ec16839813251c15c0eb8674e6e6d54ac93e7
Svetlana Domakhina <domakhina.s@gmail.com>
2023-12-26 22:03:25 -0800

    Employee1

---
+++ src/main/java/ru/netology/services/EmployeeService.java
@@ -0,0 +1,16 @@
+package ru.netology.services;
+public class EmployeeService {
+    public int calculate(int income, int expense, int threshold) {
+        int count = 0;
+        int money = 0;
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                money = (money -expense)/3;
+                count++;
+            } else {
+                money = money + income - expense;
+            }
+        }
+        return count;
+    }
+}
