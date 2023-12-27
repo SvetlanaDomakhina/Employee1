efc22bbddbd741885b662eac9c5d5b6653c3d45a
Svetlana Domakhina <domakhina.s@gmail.com>
2023-12-26 23:43:46 -0800

    EmployeeUpdated

--- src/main/java/ru/netology/services/EmployeeService.java
+++ src/main/java/ru/netology/services/EmployeeService.java
@@ -1,11 +1,12 @@
 package ru.netology.services;
+
 public class EmployeeService {
     public int calculate(int income, int expense, int threshold) {
         int count = 0;
         int money = 0;
         for (int i = 0; i < 12; i++) {
             if (money >= threshold) {
-                money = (money -expense)/3;
+                money = (money - expense) / 3;
                 count++;
             } else {
                 money = money + income - expense;
--- src/test/java/ru/netology/services/EmployeeServiceTest.java
+++ src/test/java/ru/netology/services/EmployeeServiceTest.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class EmployeeServiceTest {
-@ParameterizedTest
-@CsvFileSource(files = "src/test/resources/Employee.csv")
-public void testMonthsOff(int income, int expense, int threshold, int expected) {
-    EmployeeService service = new EmployeeService();
-    int actual = service.calculate(income, expense, threshold);
-    Assertions.assertEquals(expected, actual);
-}
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/Employee.csv")
+    public void testMonthsOff(int income, int expense, int threshold, int expected) {
+        EmployeeService service = new EmployeeService();
+        int actual = service.calculate(income, expense, threshold);
+        Assertions.assertEquals(expected, actual);
+    }
 }
